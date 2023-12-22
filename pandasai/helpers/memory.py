545687ba51d91b943c9ea903fc801685bb6eca29
louisowen6 <louisowen6@gmail.com>
2023-12-22 10:05:39 +0700

    debug

--- pandasai/helpers/memory.py
+++ pandasai/helpers/memory.py
@@ -29,8 +29,10 @@ class Memory:
         """
         Truncates the message if it is longer than max_length
         """
-        # if self.max_length is None or self.max_length < 0:
-        #     return message
+        print("MAX LENGTH IS: ",self.max_length)
+        if self.max_length is None or self.max_length < 0:
+            print("SKIP TRUNCATING")
+            return message
         return (
             f"{message[:self.max_length]} ..." if len(str(message)) > self.max_length else message
         )
